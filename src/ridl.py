e97543b57d3b401bba86bb66f1be255bfb2797ff
taiyora <taiyora@protonmail.com>
2018-02-05 17:05:21 +1300

    Implement reverse image search and download (via Google)

--- src/ridl.py
+++ src/ridl.py
@@ -1,10 +1,19 @@
+import json
 import os
 import requests
 import validators
+from bs4 import BeautifulSoup
 from colorama import init, Fore, Style
+from PIL import Image
 
 image_folder = 'images/'
 image_folder_backup = image_folder + 'backup/'
+
+headers = {}
+headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+
+google_url     = 'https://www.google.com'
+google_ris_url = 'https://www.google.com/searchbyimage?&image_url=' # ris = Reverse Image Search
 
 def is_image_url(url):
 	# First check whether the provided string is a URL at all
@@ -70,6 +79,87 @@ def download_image(url, path):
 
 	print(Fore.GREEN + 'Saved' + Style.RESET_ALL + Style.BRIGHT)
 
+	# Return the saved filename, since the image needs to be accessed later to find its size
+	return target_final.split('/')[-1]
+
+def get_page_soup(url):
+	"""Returns the page at the URL as a parsable BeautifulSoup object."""
+
+	response = requests.get(url, headers=headers)
+	print(Fore.BLACK + 'Got response: ' + str(response.status_code))
+
+	if response.status_code < 200 or response.status_code >= 300:
+		return False
+
+	return BeautifulSoup(response.text, 'lxml')
+
+def download_best_image(url, original_image_filename):
+	"""Uses Google's reverse image search to try and find a larger version of the image at the supplied URL."""
+
+	# Get the size of the original image, since we need to find only a larger one
+	original_image_path = image_folder_backup + original_image_filename
+	original_image = Image.open(original_image_path)
+
+	oiw, oih = original_image.size
+	original_image_size = oiw + oih # A simple but effective way of representing the image's size
+
+	print(Fore.WHITE + 'Original image: ' + Fore.MAGENTA + str(oiw) + 'x' + str(oih))
+
+	# Do the reverse image search
+	print(Fore.BLACK + 'Searching Google for matching images... ', end='')
+
+	results_page = get_page_soup(google_ris_url + url)
+	if not results_page:
+		print(Fore.RED + 'Failed to query Google')
+		return
+
+	other_sizes_div = results_page.find('div', id='_w6')
+	if not other_sizes_div:
+		# This div still exists even if no similar images were found, so the parsing itself must have failed
+		print(Fore.RED + 'Failed to parse results page')
+		return
+
+	other_sizes_links = other_sizes_div.find_all('a')
+	if not len(other_sizes_links):
+		print(Fore.YELLOW + "Google couldn't find any matching images")
+		return
+
+	largest_found_size_url = other_sizes_links[-1].get('href') # Will link to the Large, Medium, or Small images page
+	print(Fore.BLACK + 'Checking ' + other_sizes_links[-1].string + ' images... ', end='')
+
+	images_page = get_page_soup(google_url + largest_found_size_url)
+	if not images_page:
+		print(Fore.RED + 'Failed to query Google')
+		return
+	
+	# Form a list of the found images. We will get all the data Google provides for each image
+	images = images_page.find_all(class_='rg_meta')
+	if not images:
+		print(Fore.RED + 'Failed to parse images page')
+		return
+
+	for image in images:
+		# We can convert the image data to JSON, which will contain the image's width, height, and true URL
+		image_json = json.loads(image.string)
+
+		width     = image_json['ow']
+		height    = image_json['oh']
+		image_url = image_json['ou']
+
+		if width + height > original_image_size:
+			# Ensure that the image can be accessed, as the host could be down, or the file removed/inaccessible
+			if not is_image_url(image_url):
+				print(Fore.BLACK + 'Failed to access image URL; trying next one...')
+				continue
+
+			print(Fore.GREEN + 'Successfully found larger image: ' + Fore.MAGENTA + str(width) + 'x' + str(height))
+			download_image(image_url, image_folder)
+
+		else:
+			print(Fore.YELLOW + "Google couldn't find any larger images")
+
+		return
+
 if __name__ == '__main__':
 	import pyperclip
 	import time
@@ -90,8 +180,9 @@ if __name__ == '__main__':
 			if is_image_url(clip):
 				print(Fore.GREEN + 'Image detected!')
 
-				# First download the image to the backup folder, then try to find a larger image to save (TODO)
-				download_image(clip, image_folder_backup)
+				# First download the image to the backup folder, then try to find a larger image to save
+				original_image_filename = download_image(clip, image_folder_backup)
+				download_best_image(clip, original_image_filename)
 
 				# For clarity, leave a blank line after each reverse image download
 				print('')
